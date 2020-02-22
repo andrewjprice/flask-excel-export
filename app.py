81b3723a914f86be9037e7bdc9cacd7a2e37790d
Andrew <andrew.j.price@outlook.com>
2020-02-21 21:15:42 -0800

    Initial setup

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template
+from celery import Celery
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
